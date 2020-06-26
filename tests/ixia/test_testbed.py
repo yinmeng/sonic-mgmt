35ff1d42a7870348079d3c25469d5706b7723f4c
Wei Bai <baiwei0427@gmail.com>
2020-06-25 20:40:12 -0700

    Add fixture lossless_prio_dscp_map

--- tests/ixia/test_testbed.py
+++ tests/ixia/test_testbed.py
@@ -11,10 +11,11 @@ from common.helpers import assertions
 from lib.ixia_fixtures import ixia_api_serv_ip, ixia_api_serv_user, ixia_api_serv_passwd, ixia_dev
 from lib.ixia_helpers import get_neigh_ixia_mgmt_ip, get_neigh_ixia_card, get_neigh_ixia_port 
 from lib.common_helpers import get_vlan_subnet, get_addrs_in_subnet
+from lib.qos_fixtures import lossless_prio_dscp_map
 
 pytestmark = [pytest.mark.disable_loganalyzer]
 
-def test_testbed(testbed, conn_graph_facts, duthost, ixia_dev, ixia_api_serv_ip, ixia_api_serv_user, ixia_api_serv_passwd):
+def test_testbed(testbed, conn_graph_facts, duthost, ixia_dev, ixia_api_serv_ip, ixia_api_serv_user, ixia_api_serv_passwd, lossless_prio_dscp_map):
     print "==== testbed: {}".format(testbed)
     print "==== conn_graph_facts: {}".format(conn_graph_facts)
     print "==== IXIA API server IP: {}".format(ixia_api_serv_ip)
@@ -23,6 +24,7 @@ def test_testbed(testbed, conn_graph_facts, duthost, ixia_dev, ixia_api_serv_ip,
     print "==== IXIA chassis info: {}".format(ixia_dev)
     print "==== DUT hostname: {}".format(duthost.hostname)
     print "==== Vlan Subnet: {}".format(get_vlan_subnet(duthost))
+    print "==== Lossless priorities and DSCPs: {}".format(lossless_prio_dscp_map)
         
     print "==== Testbed info"
     device_conn = conn_graph_facts['device_conn']
